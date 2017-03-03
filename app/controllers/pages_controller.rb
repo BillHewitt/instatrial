2c2262d1255d4ee84bb13a3d794854cc90a4e24d
BillHewitt <billhewitt22@googlemail.com>
2017-03-03 10:35:44 +0000

    ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,7 @@ class PagesController < ApplicationController
 
   def login
   end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pages/new'
   get 'pages/edit'
   get 'pages/login'
+  get 'pages/show'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
